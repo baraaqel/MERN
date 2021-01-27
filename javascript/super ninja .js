725054eb137870853156e720eae727f8ca946a9e
unknown <baraaqel1998@gmail.com>
2021-01-27 15:19:56 +0300

    5th ass

--- javascript/super ninja .js
+++ javascript/super ninja .js
@@ -20,7 +20,29 @@ class Ninja {
         this.health+=10;
     }
 
+
+
 }
+
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name);
+        this._health = 200;
+        this._speed = 10;
+        this._strength = 10;
+        this._wisdom = 10;
+    }
+
+    speakWisdom() {
+        return super.drinkSake() + console.log("There are no winners. The obvious is never what it appears to be.");
+    }
+}
+
+
+
+
+
     const ninja1 = new Ninja("Hyabusa");
     ninja1.sayName();
     ninja1.showStats();
@@ -30,3 +52,8 @@ class Ninja {
     ninja2.sayName();
     ninja2.drinkSake();
     ninja2.showStats();
+
+    sensei1 = new Sensei("Ou-sama");
+    sensei1.showStats();
+    sensei1.speakWisdom();
+    sensei1.showStats();
