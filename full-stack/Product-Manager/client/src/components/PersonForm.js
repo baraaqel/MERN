8dc1583a10a26f3e190e15685965a3e8c1ea77c1
baraaqel <baraaqel1998@gmail.com>
2021-02-13 12:40:46 +0300

    update

---
+++ full-stack/Product-Manager/client/src/components/PersonForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+export default () => {
+    //keep track of what is being typed via useState hook
+    const [title, setTitle] = useState(""); 
+    const [price, setPrice] = useState("");
+    const [description, setDescription] = useState("");
+    //handler when the form is submitted
+    const onSubmitHandler = e => {
+        //prevent default behavior of the submit
+        e.preventDefault();
+        //make a post request to create a new person
+        axios.post('http://localhost:8000/api/people', {
+            title,
+            price,
+            description
+        })
+            .then(res=>console.log(res))
+            .catch(err=>console.log(err))
+    }
+    //onChange to update firstName and lastName
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <p>
+                <label>Title</label><br/>
+                <input type="text" onChange={(e)=>setTitle(e.target.value)} value={title}/>
+            </p>
+            <p>
+                <label>Price</label><br/>
+                <input type="text" onChange={(e)=>setPrice(e.target.value)} value={price}/>
+            </p>
+            <p>
+                <label>Description</label><br/>
+                <input type="text" onChange={(e)=>setDescription(e.target.value)} value={description}/>
+            </p>
+
+            <input type="submit"/>
+        </form>
+    )
+}
